0ce4285900758a95a1c40bb534d5672fe6b09213
zeshalondrag <144255214+zeshalondrag@users.noreply.github.com>
2026-05-07 22:14:03 +0300

    Create EmailVerification.cs

---
+++ Models/DTO/EmailVerification.cs
@@ -0,0 +1,8 @@
+namespace sorafix_api.Models.DTO;
+
+public class EmailVerification
+{
+    public string Code { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Type { get; set; } = null!;
+}
